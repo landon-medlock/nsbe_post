7a0877d206a0cc102e48a56ec3124d8a5d8b58eb
Landon Medlock <landon.medlock@gmail.com>
2015-03-09 20:57:27 -0500

    Adding user model and user/post relationship

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :user_name, :bio, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309233215) do
+ActiveRecord::Schema.define(version: 20150310011931) do
 
   create_table "posts", force: true do |t|
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "user_name"
+    t.string   "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
