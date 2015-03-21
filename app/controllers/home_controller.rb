65d0580df8cf51894c5856a94fe1b5f603e46fd4
Landon Medlock <landon.medlock@gmail.com>
2015-03-21 16:20:36 -0500

    Authenticating user on home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @posts = Post.all.to_a.sort! { |a, b| b.updated_at <=> a.updated_at }
     @nsbe_post = Post.new
